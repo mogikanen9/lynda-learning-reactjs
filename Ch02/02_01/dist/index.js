22c4df56d780deb914ae9e5474f2c657a32d10fc
Vlad <mogikanensoftware@gmail.com>
2017-07-24 21:03:27 -0400

    jsx syntax and babel iniline transpie introduced

--- Ch02/02_01/dist/index.js
+++ Ch02/02_01/dist/index.js
@@ -1,20 +1,13 @@
-const {createElement} = React
-
 const {render} = ReactDOM
 
-const myStyle = {
-    backgroundColor: 'orange',
-    color: 'white',
-    fontFamily: 'Verdana'
-}
-
-const title = createElement(
-    'h1',
-    {id: 'title', className:'header', style: myStyle},
-    'Hello World!'
-)
-
 render(
-    title,
+    <h1 id='title' className = 'header'
+    style={{
+        backgroundColor: 'orange',
+        color: 'white',
+        fontFamily: 'Verdana'
+        }}>
+        Hello World
+    </h1>,
     document.getElementById('react-container') 
 )
