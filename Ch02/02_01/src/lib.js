e965047937211638838afe62dc47fe5f06e2ad57
Vlad <mogikanensoftware@gmail.com>
2017-07-26 09:36:31 -0400

    used webpack for css
     * replaced iniline style with external styles
     * used css and saas styles
     * used webpack to process css and sass styles

--- Ch02/02_01/src/lib.js
+++ Ch02/02_01/src/lib.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import text from './titles.json'
+import './css/hello.css'
+import './css/goodbye.scss'
 
 export const hello = (
     <h1 id='title'
-        className='header'
-        style = {{backGroundColor: 'purple', color: 'yellow'}}>
+        className='helloStyle'>
         {text.hello}
     </h1>
 )
 
 export const goodbye = (
  <h1 id='title'
-        className='header'
-        style = {{backGroundColor: 'yellow', color: 'purple'}}>
+        className='goodByeStyle'>
         {text.goodbye}
     </h1>
 )
