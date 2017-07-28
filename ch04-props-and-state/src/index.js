60a1f036f136b8e2daf1a878d0ca910217450a10
Vlad <mogikanensoftware@gmail.com>
2017-07-28 10:30:32 -0400

    SkiDayRow and SkiDayList components
     * SkiDayList - display a lsit fo ski day rows (table)
     * SkiDayRow - displays one dat info as a table row
     * magic JSX "spread" oprator is used to pass all properties to SkiDayRow element/component

--- ch04-props-and-state/src/index.js
+++ ch04-props-and-state/src/index.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayCount } from './components/SkiDayCount'
+import { SkiDayList } from './components/SkiDayList'
 
 window.React = React
 
 render(
-    <SkiDayCount 
-            total={50}
-            powder={20}
-            backcountry={10}
-            goal={100}/>,
+    <SkiDayList 
+            days={
+                [
+                    {
+                        resort: "Snow Vallew",
+                        date: new Date("1/2/2016"),
+                        powder: true,
+                        backcountry: false
+                        },
+                    {
+                        resort: "Blue Mountain",
+                        date: new Date("10/2/2016"),
+                        powder: true,
+                        backcountry: true
+                        },
+                    {
+                        resort: "Kirkwood",
+                        date: new Date("11/2/2016"),
+                        powder: false,
+                        backcountry: false
+                        }        
+                ]
+            }/>,
     document.getElementById('react-container')
 )
