e450a98a9574524996ada9d9a753b71bb453c0c6
Vlad <mogikanensoftware@gmail.com>
2017-08-01 22:28:32 -0400

    upgraded to ES6 syntax

--- ch04-props-and-state/src/components/App.js
+++ ch04-props-and-state/src/components/App.js
@@ -1,38 +1,41 @@
-import { createClass } from 'react'
+import { Component } from 'react'
 import { SkiDayCount } from './SkiDayCount'
 import { SkiDayList } from './SkiDayList'
-export const App = createClass({
 
-        getInitialState(){
-            return {
-                allSkiDays: [
-                    {
-                        resort: "Snow Vallew",
-                        date: new Date("1/2/2016"),
-                        powder: true,
-                        backcountry: false
-                    },
-                    {
-                        resort: "Blue Mountain",
-                        date: new Date("10/2/2016"),
-                        powder: true,
-                        backcountry: true
-                    },
-                    {
-                        resort: "Kirkwood",
-                        date: new Date("11/2/2016"),
-                        powder: false,
-                        backcountry: false
-                    }
-                ]
+export class App extends Component{
+
+        constructor(props){
+            super(props)
+            this.state = {
+                    allSkiDays: [
+                                        {
+                                            resort: "Snow Vallew",
+                                            date: new Date("1/2/2016"),
+                                            powder: true,
+                                            backcountry: false
+                                        },
+                                        {
+                                            resort: "Blue Mountain",
+                                            date: new Date("10/2/2016"),
+                                            powder: true,
+                                            backcountry: true
+                                        },
+                                        {
+                                            resort: "Kirkwood",
+                                            date: new Date("11/2/2016"),
+                                            powder: false,
+                                            backcountry: false
+                                        }
+                                    ]
             }
-        },
+        }
 
        countDays(filter) {
 		const { allSkiDays } = this.state
 		return allSkiDays.filter(
 			(day) => (filter) ? day[filter] : day).length
-	},
+        }
+            
         render(){
             return (
                 <div className="app">
@@ -47,4 +50,4 @@ export const App = createClass({
                 </div>
             )
         }
-    })
+}
