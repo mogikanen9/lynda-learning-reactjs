218bea14f025c62ec63b3e29a96cd8d7294878ce
Vlad <mogikanensoftware@gmail.com>
2017-08-01 22:19:05 -0400

    using state and new App component

---
+++ ch04-props-and-state/src/components/App.js
@@ -0,0 +1,50 @@
+import { createClass } from 'react'
+import { SkiDayCount } from './SkiDayCount'
+import { SkiDayList } from './SkiDayList'
+export const App = createClass({
+
+        getInitialState(){
+            return {
+                allSkiDays: [
+                    {
+                        resort: "Snow Vallew",
+                        date: new Date("1/2/2016"),
+                        powder: true,
+                        backcountry: false
+                    },
+                    {
+                        resort: "Blue Mountain",
+                        date: new Date("10/2/2016"),
+                        powder: true,
+                        backcountry: true
+                    },
+                    {
+                        resort: "Kirkwood",
+                        date: new Date("11/2/2016"),
+                        powder: false,
+                        backcountry: false
+                    }
+                ]
+            }
+        },
+
+       countDays(filter) {
+		const { allSkiDays } = this.state
+		return allSkiDays.filter(
+			(day) => (filter) ? day[filter] : day).length
+	},
+        render(){
+            return (
+                <div className="app">
+                    <SkiDayList days={this.state.allSkiDays}/>
+                    <SkiDayCount total={this.countDays()}
+                                  powder={this.countDays(
+                                      "powder"
+                                  )}
+                                  backcountry={this.countDays(
+                                      "backcountry"
+                                  )}  />
+                </div>
+            )
+        }
+    })
--- ch04-props-and-state/src/index.js
+++ ch04-props-and-state/src/index.js
@@ -1,34 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { SkiDayList } from './components/SkiDayList'
-import { SkiDayCount } from './components/SkiDayCount'
+import { App } from './components/App'
 
 window.React = React
 
 render(
     //<SkiDayCount />,
-    <SkiDayList
-        days={
-            [
-                {
-                    resort: "Snow Vallew",
-                    date: new Date("1/2/2016"),
-                    powder: true,
-                    backcountry: false
-                },
-                {
-                    resort: "Blue Mountain",
-                    date: new Date("10/2/2016"),
-                    powder: true,
-                    backcountry: true
-                },
-                {
-                    resort: "Kirkwood",
-                    date: new Date("11/2/2016"),
-                    powder: false,
-                    backcountry: false
-                }
-            ]
-        } />,
+    <App />,
     document.getElementById('react-container')
 )
