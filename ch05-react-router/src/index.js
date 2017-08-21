0cbcb21dcd7134810ed1c78526ecedb2449660fa
Vlad <mogikanensoftware@gmail.com>
2017-08-21 10:29:27 -0400

    react-router-dom and Whoops404

--- ch05-react-router/src/index.js
+++ ch05-react-router/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { App } from './components/App'
+import { Whoops404 } from './components/Whoops404'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 window.React = React
 
 render(
     //<SkiDayCount />,
-    <App />,
+    //<App />,
+    <BrowserRouter>
+        <div>
+            <Route path = "/" component = { App } />
+            <Route path = "/*" component = { Whoops404 }/>
+        </div>
+    </BrowserRouter>,
     document.getElementById('react-container')
 )
