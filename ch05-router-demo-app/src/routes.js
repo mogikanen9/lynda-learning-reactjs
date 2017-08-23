e8ddc7ed22733f783d87ee54dc10051c62eef8f4
Vlad <mogikanensoftware@gmail.com>
2017-08-23 09:52:59 -0400

    about and memebrs added

--- ch05-router-demo-app/src/routes.js
+++ ch05-router-demo-app/src/routes.js
@@ -3,11 +3,14 @@ import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/ui/Home'
 import  { Whoops404  } from './components'
 import { About } from './components/ui/About'
+import { MemberList } from './components/ui/MemberList'
 
 const routes = (
     <Router>
         <Switch>
             <Route path="/" component={Home} />
+            <Route path="about" component={About} />
+            <Route path="members" component={MemberList} />
             <Route path="*" component={Whoops404} />
         </Switch>    
     </Router>
