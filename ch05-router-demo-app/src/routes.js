aee27febfe1ef5ec629cc840847b94a87f53c899
Vlad Voinov <mogikanen9@users.noreply.github.com>
2017-08-23 20:10:12 -0400

    Ch05 demo app (#5)
    * Rock Association Demo App init
    
    initial setup for the demo app
    
    * updated to the latest versions
    
    React and webpack were updated to the latest versions
    
    * fixed main menu
    
    * about and memebrs added
    
    * fixed routing/navigation
    
    * left adn right layout
    
    laytouts are not working - temporarily hardcoded About component into Left
    
    * Left and Right were removed
    
    Since react-router v4 is used and it does not support nested elements for Route this approach will not work - used simplified(dummy) approach

---
+++ ch05-router-demo-app/src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './components/ui/Home'
+import  { Whoops404 } from './components'
+import { About } from './components/ui/About'
+import { MemberList } from './components/ui/MemberList'
+
+const routes = (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path = "/about" component={About} />
+            <Route path="/members" component={MemberList} />
+            <Route path="*" component={Whoops404} />
+        </Switch>    
+    </Router>
+)
+
+export default routes
